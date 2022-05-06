eca5d4e2570e3fefd56941deae21382687763af5
Alena Chia <105028037+alenachia@users.noreply.github.com>
2022-05-06 10:11:18 +0800

    First upload

--- nav.php
+++ nav.php
@@ -1,3 +1,9 @@
+<!-- Done by Alena -->
+
+<html>
+<meta charset = "utf-8">
+<link href="header_style.css" rel="stylesheet" type= "text/css">
+<link href="content_style.css" rel="stylesheet" type= "text/css">
 <h1>Police Emergency Service System</h1>
 <nav id = "HeaderStyle">
 	<ul>
